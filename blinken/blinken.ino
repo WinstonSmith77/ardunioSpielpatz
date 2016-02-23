6385e45d3dd4556ee2d761c08db463b31e23e770
Matthias Henning <Matze_GitHub@gmx.de>
2016-02-23 21:21:42 +0100

    sos

--- blinken/blinken.ino
+++ blinken/blinken.ino
@@ -1,4 +1,6 @@
 const int portToUse = 13;
+const int WAIT = 500;
+const int SCALEDOWN = 2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,12 +8,18 @@ void setup() {
 }
 
 void loop() {
-  turnOnFor(1000);
-  digitalWrite(portToUse, LOW);
-  delay(1000);
+  int steps[] = {500, 500, 500, 1000, 1000, 1000, 500, 500, 500};
+  for(int i = 0; i < sizeof(steps) / sizeof(int); i++)
+  {
+   turnOnFor(steps[i]);
+  }
+  delay(4000 / SCALEDOWN);
 }
 
 void turnOnFor(int time){
+  
   digitalWrite(portToUse, HIGH);
-  delay(time);
+  delay(time / SCALEDOWN);
+  digitalWrite(portToUse, LOW);
+  delay(WAIT / SCALEDOWN);
 }
